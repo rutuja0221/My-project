e0dc3f47fabfb78cc9d6b635a5e5d32e505ed086
rutuja0221 <rutumandhare02@gmail.com>
2025-05-28 12:40:50 +0530

    Add files via upload

---
+++ Ascii.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	 char ch;
+	  
+	  printf(" enter your character =");
+	  scanf("%c",&ch);
+	  printf(" ASCII value of character=%d",ch);
+	  
+	  
+	  return 0;
+}
